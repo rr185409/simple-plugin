4693cff0481e359daad6ad5a48da90e0b598a12b
Ristovski <rr185409@ncr.com>
2023-09-19 14:22:24 +0200

    init commit

---
+++ src/main/java/com/ncr/plugins/SimpleOperations.java
@@ -0,0 +1,12 @@
+package com.ncr.plugins;
+
+public class SimpleOperations {
+	public int add(int a, int b) {
+		return a+b;
+	}
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+	
+
+}
---
+++ src/main/java/com/ncr/plugins/SimplePluginMain.java
@@ -0,0 +1,10 @@
+package com.ncr.plugins;
+
+public class SimplePluginMain {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+
+	}
+
+}
